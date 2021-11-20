441ee0f39e5eb7d689ccf30b276c4b446e449068
ilonaand <ilonaand@gmail.com>
2021-11-20 12:30:55 +0300

    feat: index

---
+++ my_ciphering_cli.js
@@ -0,0 +1,50 @@
+import { unFlat, validateConfig } from './validate.js';
+import  process, { stdin, stdout } from 'process';
+import { pipeline } from  'stream';
+import { myTransform } from './transformStream.js';
+import { myReadStream } from './readableStream.js';
+import { myWriteStream } from './writableStream.js';
+import path from 'path';
+const __dirname = path.resolve();
+
+const flagArray = process.argv.slice(2);
+const allowFlags = [ '-c', '--config', '-i', '--input', '-o', '--output' ];
+const allowConfig = [ 'C0', 'C1', 'R0', 'R1', 'A'];
+const obj = unFlat(flagArray, allowFlags);
+const config = obj['c'];
+const arrConfig = validateConfig(config, allowConfig);
+const configMap = {
+    'C0': () => new myTransform({ shift: 1, direction: 0}),
+    'C1': () => new myTransform({ shift: 1, direction: 1}),
+    'R0': () => new myTransform({ shift: 8, direction: 0}),
+    'R1': () => new myTransform({ shift: 8, direction: 1}),
+    'A': () => new myTransform({ shift: -1, direction: 0}),
+  };
+//console.log(flagArray, obj, arrConfig); 
+const read = obj["i"] ? new myReadStream(path.join(__dirname, obj["i"])) : stdin;
+read.on("error", err => {
+  console.error(err);
+  process.exit(1);
+});
+  
+const write = obj["o"] ? new myWriteStream(path.join(__dirname, obj["o"])) : stdout;
+write.on("error", err => {
+  console.error(err);
+  process.exit(1);
+});
+
+const arrTransform = arrConfig.map((item) => configMap[item]()); 
+
+try {
+  pipeline(
+    read,
+    ... arrTransform,
+    write,
+    err => err
+  );
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+};
+
+ 
